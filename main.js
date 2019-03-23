14be7c8b566a72eefbdad7472c252e71a443711b
Spencer Owen <owenspencer@gmail.com>
2019-03-22 23:09:21 -0600

    adds css

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 400, height: 400 })
 
   // and load the index.html of the app.
   win.loadFile('index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
