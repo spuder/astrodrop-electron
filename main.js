68914964813ec83de968b9d2ae01f3a60c6ab33b
Spencer Owen <owenspencer@gmail.com>
2019-03-26 23:46:25 -0600

    add index.html

--- main.js
+++ main.js
@@ -78,3 +78,4 @@ const showWindow = () => {
   window.show()
   window.focus()
 }
+
--- renderer.js
+++ renderer.js
@@ -1,3 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+function upload() {
+  let file = document.getElementById("submitFile");
+  console.log(file)
+  let req = new XMLHttpRequest();
+  let formData = new FormData();
+  formData.append("file", file);
+  req.open("POST", "https://1b3f69e9-47db-4e0e-8641-8d63257dca60.mock.pstmn.io/api/files/local");
+  req.send(formData);
+}
+
+document.querySelector('#submitFile').addEventListener('click', () => {
+    upload()
+})
