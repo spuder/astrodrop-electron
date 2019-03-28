c3ec6b21b4f6497144f4eba1cacbdcfd71408ef7
Spencer Owen <owenspencer@gmail.com>
2019-03-27 23:33:30 -0600

    trying POST request

--- renderer.js
+++ renderer.js
@@ -3,13 +3,19 @@
 // All of the Node.js APIs are available in this process.
 
 function upload() {
-  let file = document.getElementById("submitFile");
-  console.log(file)
+  let file = document.getElementById("file").value;
+  let apikey = document.getElementById("apikey").value;
+  console.log("file is " + file);
+  console.log("apikey is " + apikey);
   let req = new XMLHttpRequest();
+  req.withCredentials = true;
   let formData = new FormData();
   formData.append("file", file);
-  req.open("POST", "https://1b3f69e9-47db-4e0e-8641-8d63257dca60.mock.pstmn.io/api/files/local");
+  formData.append("print", "0");
+  req.open("POST", "http://astrobox1.local/api/files/local");
+  req.setRequestHeader("X-Api-Key", apikey);
   req.send(formData);
+  console.log(req.response);
 }
 
 document.querySelector('#submitFile').addEventListener('click', () => {
