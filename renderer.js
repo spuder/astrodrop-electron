d718279ca7971d535f24e2694e52a429ce84fe13
Spencer Owen <owenspencer@gmail.com>
2019-03-31 00:01:10 -0600

    Fix duplicate ids

--- renderer.js
+++ renderer.js
@@ -5,28 +5,26 @@
 printer_status()
 
 function printer_state() {
-  let file = document.getElementById("file").value;
+  let printer = document.getElementById("printer").value;
   let apikey = document.getElementById("apikey").value;
-  console.log("file is " + file);
-  console.log("apikey is " + apikey);
   let req = new XMLHttpRequest();
   req.withCredentials = true;
   let formData = new FormData();
-  req.open("GET", "http://astrobox1.local/api/printer");
+  req.open("GET", `http://${printer}/api/printer`);
   req.setRequestHeader("X-Api-Key", apikey);
   req.send(formData);
   console.log(req.readyState);
   return req.readyState;
-  #TODO: what if 404 or not available
+  // TODO: what if 404 or not available
 }
 
 function printer_status(status) {
+  // TODO: This makes a new circle every time, doesn't move dynamically with window
   var c = document.getElementById("printer_status");
   var ctx = c.getContext("2d");
   ctx.beginPath();
   ctx.arc(window.innerWidth/2,9,6,0, 2 * Math.PI);
   ctx.stroke();
-  console.log("status is " + status)
   if (status === 1 ) {
     ctx.fillStyle = "green";
     ctx.fill();
